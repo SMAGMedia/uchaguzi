c5ee5df3a89498c6748805672851aefafc13f634
jdbrasco <sandojohnson@hotmail.com>
2014-01-22 18:10:06 -0600

    Tessting Git upload

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ $kohana_application = '/var/www/html/application';
  * by your application. Modules are enabled and disabled by the application
  * configuration file.
  *
- * This path can be absolute or relative to this file.
+ * This path can be absolute or relative to this file. Sando
  */
 $kohana_modules = '/var/www/html/modules';
 
